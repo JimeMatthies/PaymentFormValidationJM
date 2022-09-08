3677e1553a0e0dd297717012bd7c7042c0c55e5b
Jime Matthies <JimeMatthies@gmail.com>
2022-09-08 15:36:02 -0300

    Formulario Validado

--- src/app.js
+++ src/app.js
@@ -2,25 +2,50 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-let loginForm = document.querySelector("#loginForm");
-loginForm.addEventListener("submit", evento => {
+let paymentform = document.querySelector("#paymentform");
+paymentform.addEventListener("submit", evento => {
   evento.preventDefault();
 
   let errors = {};
 
-  let rgxValidPassword = /^(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+  let RegexOnlyLetter = /^[a-zA-Z]+$/;
+  let RegexCardNumber = /^[0-9]{16}$/;
+  let RegexCardCode = /^(\d{3}|\d{4})$/;
 
-  let cnnFeedback = document.querySelector("#cardFeedback");
-  let cvcFeedback = document.querySelector("#cvcFeedback");
-  let amountFeedback = document.querySelector("#amountFeedback");
-  let firstnameFeedback = document.querySelector("#firstnameFeedback");
-  let lastnameFeedback = document.querySelector("#lastnameFeedback");
-  let cityFeedback = document.querySelector("#cityFeedback");
-  let stateFeedback = document.querySelector("#stateFeedback");
-  let zipFeedback = document.querySelector("#zipFeedback");
+  let ccnNegativeFeedback = document.querySelector("#ccnNegativeFeedback");
+  let ccnPositiveFeedback = document.querySelector("#ccnPositiveFeedback");
+  let cvcNegativeFeedback = document.querySelector("#cvcNegativeFeedback");
+  let cvcPositiveFeedback = document.querySelector("#cvcPositiveFeedback");
+  let amountNegativeFeedback = document.querySelector(
+    "#amountNegativeFeedback"
+  );
+  let amountPositiveFeedback = document.querySelector(
+    "#amountPositiveFeedback"
+  );
+  let firstnameNegativeFeedback = document.querySelector(
+    "#firstnameNegativeFeedback"
+  );
+  let firstnamePositiveFeedback = document.querySelector(
+    "#firstnamePositiveFeedback"
+  );
+  let lastnameNegativeFeedback = document.querySelector(
+    "#lastnameNegativeFeedback"
+  );
+  let lastnamePositiveFeedback = document.querySelector(
+    "#lastnamePositiveFeedback"
+  );
+  let cityNegativeFeedback = document.querySelector("#cityNegativeFeedback");
+  let cityPositiveFeedback = document.querySelector("#cityPositiveFeedback");
+  let stateNegativeFeedback = document.querySelector("#stateNegativeFeedback");
+  let statePositiveFeedback = document.querySelector("#statePositiveFeedback");
+  let zipNegativeFeedback = document.querySelector("#zipNegativeFeedback");
+  let zipPositiveFeedback = document.querySelector("#zipPositiveFeedback");
+  let messageNegativeFeedback = document.querySelector(
+    "#messageNegativeFeedback"
+  );
+  let messagePositiveFeedback = document.querySelector(
+    "#messagePositiveFeedback"
+  );
 
   let {
     ccn,
@@ -30,83 +55,156 @@ loginForm.addEventListener("submit", evento => {
     lastname,
     city,
     state,
-    zip
+    zip,
+    message
   } = evento.target;
 
   if (ccn.value === "") {
-    errors["ccn"] = "Card no puede estar vacio";
+    errors["ccn"] = "Debe ingresar un número de tarjeta";
     ccn.classList.add("is-invalid");
+    ccn.classList.remove("is-valid");
+  } else if (!RegexCardNumber.test(ccn.value)) {
+    errors["ccn"] = "Numero de tarjeta inválido";
+    ccn.classList.add("is-invalid");
+    ccn.classList.remove("is-valid");
   } else {
     ccn.classList.remove("is-invalid");
-    delete errors["ccn"];
+    ccn.classList.add("is-valid");
+    errors["ccn"] = "Tarjeta válida!";
   }
 
   if (cvc.value === "") {
-    errors["cvc"] = "cvc no puede estar vacio";
+    errors["cvc"] = "Debe ingresar código de verificación";
     cvc.classList.add("is-invalid");
+    cvc.classList.remove("is-valid");
+  } else if (!RegexCardCode.test(cvc.value)) {
+    errors["cvc"] = "Código inválido";
+    cvc.classList.add("is-invalid");
+    cvc.classList.remove("is-valid");
   } else {
     cvc.classList.remove("is-invalid");
-    delete errors["cvc"];
+    cvc.classList.add("is-valid");
+    errors["cvc"] = "Código válido!";
   }
 
   if (amount.value === "") {
-    errors["amount"] = "amount no puede estar vacio";
+    errors["amount"] = "Debe ingresar una cantidad";
     amount.classList.add("is-invalid");
+    amount.classList.remove("is-valid");
   } else {
     amount.classList.remove("is-invalid");
-    delete errors["amount"];
+    amount.classList.add("is-valid");
+    errors["amount"] = "Cantidad válida!";
   }
 
   if (firstname.value === "") {
-    errors["firstname"] = "firstname no puede estar vacio";
+    errors["firstname"] = "Su nombre no puede estar vacio";
     firstname.classList.add("is-invalid");
+    firstname.classList.remove("is-valid");
+  } else if (!RegexOnlyLetter.test(firstname.value)) {
+    errors["firstname"] = "Nombre debe contener solo letras";
+    firstname.classList.add("is-invalid");
+    firstname.classList.remove("is-valid");
   } else {
     firstname.classList.remove("is-invalid");
-    delete errors["firstname"];
+    firstname.classList.add("is-valid");
+    errors["firstname"] = "Nombre válido!";
   }
 
   if (lastname.value === "") {
-    errors["lastname"] = "lastname no puede estar vacio";
+    errors["lastname"] = "Su apellido no puede estar vacio";
     lastname.classList.add("is-invalid");
+    lastname.classList.remove("is-valid");
+  } else if (!RegexOnlyLetter.test(lastname.value)) {
+    errors["lastname"] = "Apellido debe contener solo letras";
+    lastname.classList.add("is-invalid");
+    lastname.classList.remove("is-valid");
   } else {
     lastname.classList.remove("is-invalid");
-    delete errors["lastname"];
+    lastname.classList.add("is-valid");
+    errors["lastname"] = "Apellido válido!";
   }
 
   if (city.value === "") {
-    errors["city"] = "city no puede estar vacio";
+    errors["city"] = "Su ciudad no puede estar vacia";
     city.classList.add("is-invalid");
+    city.classList.remove("is-valid");
+  } else if (!RegexOnlyLetter.test(city.value)) {
+    errors["city"] = "Ciudad debe contener solo letras";
+    city.classList.add("is-invalid");
+    city.classList.remove("is-valid");
   } else {
     city.classList.remove("is-invalid");
-    delete errors["city"];
+    city.classList.add("is-valid");
+    errors["city"] = "Ciudad válida!";
   }
 
-  if (state.value === "") {
-    errors["state"] = "state no puede estar vacio";
+  if (state.value === "Pick a state") {
+    errors["state"] = "Su estado no puede estar vacio";
     state.classList.add("is-invalid");
+    state.classList.remove("is-valid");
   } else {
     state.classList.remove("is-invalid");
-    delete errors["state"];
+    state.classList.add("is-valid");
+    errors["state"] = "Estado válido!";
   }
 
   if (zip.value === "") {
-    errors["zip"] = "zip no puede estar vacio";
+    errors["zip"] = "El código postal no puede estar vacio";
     zip.classList.add("is-invalid");
+    zip.classList.remove("is-valid");
   } else {
     zip.classList.remove("is-invalid");
-    delete errors["zip"];
+    zip.classList.add("is-valid");
+    errors["zip"] = "Código postal válido!";
   }
 
-  if (errors["card"]) {
+  if (message.value === "") {
+    errors["message"] = "Debe ingresar mensaje";
+    message.classList.add("is-invalid");
+    message.classList.remove("is-valid");
+  } else {
+    message.classList.remove("is-invalid");
+    message.classList.add("is-valid");
+    errors["message"] = "Mensaje válido!";
+  }
+
+  if (
+    errors["ccn"] ||
+    errors["cvc"] ||
+    errors["amount"] ||
+    errors["firstname"] ||
+    errors["lastname"] ||
+    errors["city"] ||
+    errors["state"] ||
+    errors["zip"] ||
+    errors["message"]
+  ) {
     console.log(errors);
-    if (errors["ccn"]) ccnFeedback.innerHTML = errors["ccn"];
-    if (errors["cvc"]) cvcFeedback.innerHTML = errors["cvc"];
-    if (errors["amount"]) amountFeedback.innerHTML = errors["amount"];
-    if (errors["firstname"]) firstnameFeedback.innerHTML = errors["firstname"];
-    if (errors["lastname"]) lastnameFeedback.innerHTML = errors["lastname"];
-    if (errors["city"]) cityFeedback.innerHTML = errors["city"];
-    if (errors["state"]) stateFeedback.innerHTML = errors["state"];
-    if (errors["zip"]) zipFeedback.innerHTML = errors["zip"];
+    if (errors["ccn"]) ccnNegativeFeedback.innerHTML = errors["ccn"];
+    if (errors["ccn"]) ccnPositiveFeedback.innerHTML = errors["ccn"];
+    if (errors["cvc"]) cvcNegativeFeedback.innerHTML = errors["cvc"];
+    if (errors["cvc"]) cvcPositiveFeedback.innerHTML = errors["cvc"];
+    if (errors["amount"]) amountNegativeFeedback.innerHTML = errors["amount"];
+    if (errors["amount"]) amountPositiveFeedback.innerHTML = errors["amount"];
+    if (errors["firstname"])
+      firstnameNegativeFeedback.innerHTML = errors["firstname"];
+    if (errors["firstname"])
+      firstnamePositiveFeedback.innerHTML = errors["firstname"];
+    if (errors["lastname"])
+      lastnameNegativeFeedback.innerHTML = errors["lastname"];
+    if (errors["lastname"])
+      lastnamePositiveFeedback.innerHTML = errors["lastname"];
+    if (errors["city"]) cityNegativeFeedback.innerHTML = errors["city"];
+    if (errors["city"]) cityPositiveFeedback.innerHTML = errors["city"];
+    if (errors["state"]) stateNegativeFeedback.innerHTML = errors["state"];
+    if (errors["state"]) statePositiveFeedback.innerHTML = errors["state"];
+    if (errors["zip"]) zipNegativeFeedback.innerHTML = errors["zip"];
+    if (errors["zip"]) zipPositiveFeedback.innerHTML = errors["zip"];
+    if (errors["message"])
+      messageNegativeFeedback.innerHTML = errors["message"];
+    if (errors["message"])
+      messagePositiveFeedback.innerHTML = errors["message"];
   } else {
     evento.target.submit();
   }
